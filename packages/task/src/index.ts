c5ae26705ddd85ab6ff4eb087fa57107dfa5f6c2
utlime <utlime@gmail.com>
2018-09-20 21:59:37 +0700

    init

---
+++ packages/task/src/index.ts
@@ -0,0 +1,41 @@
+/**
+ * Task with name and status
+ */
+export interface Task {
+  task: string;
+  done: boolean;
+}
+
+/**
+ * Create new task with default options
+ */
+export function defaults(
+  options: { done?: boolean } = { done: false }
+): (task: { task: string; done?: boolean }) => Task {
+  return task => ({
+    done: task.done || options.done || false,
+    task: task.task,
+  });
+}
+
+/**
+ * Return boolean value if task status is equal to done
+ */
+export function isDone(done: boolean): (task: Task) => boolean {
+  return task => task.done === done;
+}
+
+/**
+ * Create new task with option
+ */
+export function update(
+  options: {
+    task?: string;
+    done?: boolean;
+  } = {}
+): (task: Task) => Task {
+  return task => ({
+    task: options.task || task.task,
+    done: options.done || task.done,
+  });
+}
