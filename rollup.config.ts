ebff4b6e85b27a3dc7a4dda349e3daabd04eb0b8
utlime <utlime@gmail.com>
2018-10-16 21:59:06 +0700

    chore(build): cleanup rollup config
    affects: @utlime/identity, @utlime/sort, @utlime/tag, @utlime/task-list, @utlime/task

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,9 +1,10 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
-import { camelCase, upperFirst } from 'lodash';
 import typescript from 'rollup-plugin-typescript2';
 import { uglify } from 'rollup-plugin-uglify';
+import clear from 'rollup-plugin-clear'
+import { camelCase, upperFirst } from 'lodash';
 // @ts-ignore
 import path from 'path';
 
@@ -15,30 +16,32 @@ const globals = Object.keys(pkg.peerDependencies || {}).reduce((acc, cur) => ({ 
 
 export default [
   {
-    input: path.join(ROOT, 'src/index.ts'),
     output: [
-      { file: path.join(ROOT, pkg.main), format: 'umd', sourcemap: true, name: nameBuilder(pkg.name), globals },
-      { file: path.join(ROOT, pkg.module), format: 'es', sourcemap: true },
+      { file: pkg.main, format: 'umd', sourcemap: true, name: nameBuilder(pkg.name), globals },
+      { file: pkg.module, format: 'es', sourcemap: true, name: nameBuilder(pkg.name), globals },
     ],
-    external: [...Object.keys(globals)],
     plugins: [
-      typescript({ useTsconfigDeclarationDir: true }),
-      commonjs(),
-      resolve({
-        customResolveOptions: {
-          moduleDirectory: 'node_modules',
-        },
-      }),
       sourceMaps(),
     ],
   },
   {
-    input: path.join(ROOT, 'src/index.ts'),
     output: [
-      { file: path.join(ROOT, pkg.unpkg), format: 'iife', sourcemap: true, name: nameBuilder(pkg.name), globals },
+      { file: pkg.unpkg, format: 'iife', sourcemap: true, name: nameBuilder(pkg.name), globals },
     ],
-    external: [...Object.keys(globals)],
     plugins: [
+      uglify(),
+    ],
+  },
+].map(({ output, plugins }) => {
+  return {
+    input: path.join(ROOT, 'src/index.ts'),
+    external: Object.keys(globals),
+    output: output.map((out) => ({
+      ...out,
+      file: path.join(ROOT, out.file)
+    })),
+    plugins: [
+      clear({ targets: [path.join(ROOT, 'dist')] }),
       typescript({ useTsconfigDeclarationDir: true }),
       commonjs(),
       resolve({
@@ -46,7 +49,7 @@ export default [
           moduleDirectory: 'node_modules',
         },
       }),
-      uglify(),
-    ],
-  },
-];
+      ...plugins
+    ]
+  }
+});
