bcb8ae385537f66833ed92eba268e0b9e46b2df1
utlime <utlime@gmail.com>
2018-10-28 11:18:56 +0700

    chore(task-list): small fixes
    affects: @utlime/task-list

--- packages/task-list/src/ListTag.ts
+++ packages/task-list/src/ListTag.ts
@@ -3,10 +3,3 @@ import { Comparable } from '@utlime/sort';
 import { Tag } from '@utlime/tag';
 
 export interface ListTag extends Tag, Identity, Comparable {}
-
-const x: ListTag = {
-  id: 'qwe',
-  tag: 'qwe',
-  weight: 1,
-  color: 'qwe'
-};
