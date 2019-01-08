78e7d9bce8fe2419f24026f97edda8ae39fc27a1
utlime <utlime@gmail.com>
2019-01-08 22:45:42 +0700

    chore(storybook): ui storybook
    affects: @utlime/ui

--- packages/ui/storybook/webpack.config.js
+++ packages/ui/storybook/webpack.config.js
@@ -1,10 +1,16 @@
 module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    use: [{
-      loader: require.resolve('awesome-typescript-loader')
-    }]
+    use: [
+      {
+        loader: require.resolve('awesome-typescript-loader'),
+        options: {
+          configFileName: 'tsconfig.storybook.json',
+        },
+      },
+    ],
   });
+
   config.resolve.extensions.push('.ts', '.tsx');
 
   return config;
